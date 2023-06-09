5f0cf3d4d890cc5008a32d8a0755e1f5932455bf
OMJS2769 <mauricio2769@outlook.es>
2023-06-09 14:46:12 -0600

    add tabs to edit and show ticket

---
+++ resources/views/ticket/expensives_edit.blade.php
@@ -0,0 +1,2 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae explicabo esse dolore! Amet distinctio in, a dignissimos
+voluptatibus omnis est aperiam, magnam dolores tempora sed rerum ducimus voluptate, maxime ex?
---
+++ resources/views/ticket/expensives_show.blade.php
@@ -0,0 +1,2 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ducimus consequatur iusto, nam doloribus ratione officiis
+fuga quis ullam esse sit fugit dignissimos sequi laudantium ea! Dolorum maiores repellendus aspernatur.
